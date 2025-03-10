485b22745b2711b23c83c3be9a6b5870e9a0a662
ispiryan.greg@yandex.ru <ispiryan.greg@yandex.ru>
2025-03-10 15:29:22 +1000

    доделал доконца

--- WebApplication30/Controllers/PageController .cs
+++ WebApplication30/Controllers/PageController .cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace WebApplication30.Controllers
 {
     public class PageController : Controller
     {
         public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        [Route("Page/Welcome/{name}")]
+        public IActionResult Welcome(string name)
+        {
+            ViewData["Name"] = name; 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit (string message)
+        {
+            ViewData["Message"] = message;
+            return View("EditResult");
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
         {
             return View();
         }
