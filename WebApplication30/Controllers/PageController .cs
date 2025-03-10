671f2066000f710f11f2c9266597c49c7210ba48
Grisha <Grisha@DESKTOP-VPV9CV8>
2025-03-10 14:00:38 +1000

    Добавьте файлы проекта.

---
+++ WebApplication30/Controllers/PageController .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication30.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
